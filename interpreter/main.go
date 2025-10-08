507b620a9cc4853946a5f66915c12933d51b1bdf
Dominic Kolp <stupidlilgoober@gmail.com>
2025-10-08 07:27:49 -0400

    Create main.go

---
+++ interpreter/main.go
@@ -0,0 +1,10 @@
+// THIS FILE CONTAINS THE "MAIN" KNOWLEDGE
+// FOR THIS INTERPRETER.
+
+var docsLink = "https://GitHub.com/NCSources0/NaNCode/docs"
+
+import("fmt") // for formatted I/O operations
+
+func main() {
+  fmt.Println("NaNCode | By Dominic Kolp and NCSources")
+}
