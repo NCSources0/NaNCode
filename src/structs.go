2771eb16ae0f65b94cb27a73c975c60d8c83c448
NCSources0 <ncsources0@gmail.com>
2025-10-09 18:42:11 -0400

    Remove main.go

--- src/structs.go
+++ src/structs.go
@@ -1,1 +1,16 @@
 package main
+
+import ("regexp");
+
+var keywords = []any{"null", "NaN", "#", "true", "false", "undefined", "if", "else", "function", "return", "try", "catch", "finally", "new"};
+var functions = []any{"print", "!"};
+var operators = []any{"==", ">=", "<=", ">", "<"};
+var variables = map[any]any{};
+var regex = map[any]*regexp.Regexp{
+  "var": regexp.MustCompile(`\s*(.*?)\s*=\s*(.*)`),
+  "string0": regexp.MustCompile(`"(.*?)"`),
+  "string1": regexp.MustCompile(`'(.*?)'`),
+  "array": regexp.MustCompile(`^\[.*]$`),
+  "object": regexp.MustCompile(`^{.*}$`),
+  "number": regexp.MustCompile(`^\d+$`),
+};
