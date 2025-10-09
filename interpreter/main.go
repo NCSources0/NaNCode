5a0b2d7e8a9540e2bf21a83ed3da8830376184a1
NCSources0 <ncsources0@gmail.com>
2025-10-09 17:30:18 -0400

    Update main.js, making it unusable-ish (4 now)

--- interpreter/main.go
+++ interpreter/main.go
@@ -1,10 +1,30 @@
-// THIS FILE CONTAINS THE "MAIN" KNOWLEDGE
-// FOR THIS INTERPRETER.
+package main
 
-var docsLink = "https://GitHub.com/NCSources0/NaNCode/docs"
+import (
+	"fmt"
+	"regexp"
+);
 
-import("fmt") // for formatted I/O operations
+var keywords = []any{"null", "NaN", "#", "true", "false", "undefined", "if", "else", "function", "return", "try", "catch", "finally", "new"};
+var functions = []any{"print", "!"};
+var operators = []any{"==", ">=", "<=", ">", "<"};
+var variables = map[any]any{};
+var regex = map[any]*regexp.Regexp{
+  "var": regexp.MustCompile(`\s*(.*?)\s*=\s*(.*)`),
+  "string0": regexp.MustCompile(`"(.*?)"`),
+  "string1": regexp.MustCompile(`'(.*?)'`),
+  "array": regexp.MustCompile(`^\[.*]$`),
+  "object": regexp.MustCompile(`^{.*}$`),
+  "number": regexp.MustCompile(`^\d+$`),
+};
 
 func main() {
-  fmt.Println("NaNCode | By Dominic Kolp and NCSources")
+  fmt.Println("NaNCode | By Dominic Kolp and NCSources");
+  fmt.Println("For documentation, visit https://GitHub.com/NCSources0/NaNCode/docs");
+  fmt.Println(regex["var"].FindStringSubmatch(" a = 5 ")[2]);
+  fmt.Println(regex["number"].FindStringSubmatch(" 42 ")[0]);
+  fmt.Println(regex["string0"].FindStringSubmatch(" \"hello\" ")[1]);
+  fmt.Println(regex["string1"].FindStringSubmatch(" 'hello' ")[1]);
+  fmt.Println(regex["array"].FindStringSubmatch(" [1, 2, 3] ")[1]);
+  fmt.Println(regex["object"].FindStringSubmatch(" {key: value} ")[1]);
 }
--- jscompiler.js
+++ jscompiler.js
@@ -10,50 +10,89 @@ you can search for (CTRL+F) for "////"
 // LANGAUGE CAN BE USED FOR THE WEB.
 
 //// THE BASICS
-const keywords = ['null', 'NaN', '#', 'true', 'false', 'undefined', 'if', 'else', 'function', 'return', 'try', 'catch', 'finally', 'new'];
+const keywords = ['null', 'NaN', '#', 'true', 'false', 'undefined', 'if', 'else', 'return'];
 const functions = ['print', '!'];
 const operators = ['==', '>=', '<=', '>', '<'];
-const declare = /\s*(.*?)\s*=\s*(.*)/;
-const string = /^(["'])(.*)\1$/m;
-const array = /^\[.*]$/m;
-const json = /^{.*}$/m;
-const number = /^\d+$/m;
+
+const regex = {
+  var: /([a-zA-Z0-9]+)\s*=\s*(.*)/,
+  string: /^(["'])(.*?)\1$/m,
+  array: /^\[.*]$/m,
+  json: /^{.*}$/m,
+  number: /^\d+(?:\.\d+)?$/m,
+  stringIdentifier: /'(.*?)(?<!\\)'/g,
+  setKey: /([a-zA-Z0-9]+)\s*\[\s*(.+)]\s*=\s*(.*)/
+};
+
 let variables = {};
 
 function tokenize(str) {
   str.split('\n').forEach(line => {
-    if (declare.test(line)) {
-      let match = declare.exec(line).slice(1, 3);
+    line = line.trim();
+    if (regex.var.test(line)) {
+      let match = regex.var.exec(line).slice(1, 3);
 
       const varName = match[0];
 
       if (keywords.includes(varName) || functions.includes(varName) || operators.includes(varName)) return;
 
       let value = match[1];
-      let type = 'unknown';
 
-      if (string.test(value)) {
-        value = string.exec(value)[2];
-        type = 'string';
-      } else if (array.test(value)) {
-        value = JSON.parse(value);
-        type = 'array';
-      } else if (json.test(value)) {
-        value = JSON.parse(value);
-        type = 'object';
-      } else if (number.test(value)) {
-        value = parseFloat(value);
-        type = 'number';
+      value = value.replaceAll(regex.stringIdentifier, (_, inside) => `"${inside}"`);
+
+      console.log(varName, value);
+
+      const formatted = format(value);
+      if (formatted.type === 'object') {
+        for (const key in formatted.value) {
+          variables[`${varName}[${key}]`] = format(`"${formatted.value[key]}"`);
+        }
+      } else if (formatted.type === 'array') {
+        for (let i = 0; i < formatted.value.length; i++) {
+          variables[`${varName}[${i}]`] = format(`"${formatted.value[i]}"`);
+        }
+      } else {
+        variables[varName] = formatted;
       }
+    } else if (regex.setKey.test(line)) {
+      let match = regex.setKey.exec(line).slice(1, 4);
 
-      variables[varName] = {value, type};
+      const varName = match[0];
+      const key = match[1];
+      let value = match[2];
+
+      if (!(varName in variables)) return;
+      if (variables[varName].type !== 'object' && variables[varName].type !== 'array') return;
+
+
     }
   });
 }
 
-tokenize(`x = "67"
-y = [6, 7]
+function format(value) {
+  value = value.trim();
+  let type = 'unknown';
+  if (regex.string.test(value)) {
+    value = regex.string.exec(value)[2];
+    type = 'string';
+  } else if (regex.array.test(value)) {
+    value = JSON.parse(value);
+    type = 'array';
+  } else if (regex.json.test(value)) {
+    value = JSON.parse(value);
+    type = 'object';
+  } else if (regex.number.test(value)) {
+    value = parseFloat(value);
+    type = 'number';
+  }
+  return {value, type};
+}
+
+tokenize(`hello = 'world'
+hi = ['y', 'o', 'u']
 z = { "a": 6, "b": 7 }
-n = 67
+y = { "a": "a", "b": [6] }
+int = 67
+float = 6.7
 z['a'] = 8`)
 console.log(variables);
